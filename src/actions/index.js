5bfe9afe7cc44091aec6e4762a9c23f2c52b33fe
Zejian Shen <zesansan@gmail.com>
2018-03-20 22:22:42 -0700

    can make api call from search bar

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,14 @@
+import axios from 'axios';
+
 const API_KEY = '93ce2b122b5f57eabbb564fc8a4f7249';
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
